59cbb167941c6a3a6062e8babb0934e4f320c2eb
Edilson Santos <edilson0982021@gmail.com>
2022-09-19 10:33:03 -0300

    features added to the project.

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as C from './styles';
+
+const Header = () => {
+  return(
+    <C.container>
+      <C.Header>
+        <C.Title>Controle Financeiro</C.Title>
+      </C.Header>
+    </C.container>
+  );
+}
+
+export default Header;
